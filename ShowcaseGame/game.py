305b0ba8cf969f1fb1eb08f0c10e89b979186b90
JXSnack <jx.snack@gmail.com>
2023-03-06 17:51:11 +0100

    v1.0.1 Image bugfix and remove of Showcase Plugin

--- ShowcaseGame/game.py
+++
@@ -1,9 +0,0 @@
-import crys.crystal
-from crys.crystal import BuilderType
-
-game = {"info": {"name": "test", "authors": ["JX_Snack"], "out": "out/"},
-		"scenes": [{"title": "Papa", "buttons": [["Go to scen", 1]]},
-				   {"title": "Scene 2", "buttons": [["Go to scene 1", 1], ["Go to scene 3", 3]]},
-				   {"title": "Scene 3", "buttons": [["Go to scene 1", 1], ["Go to scene 2", 2]]}]}
-
-crys.crystal.Game(game, BuilderType.HTMLPlus).build()
